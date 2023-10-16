add839ecddd0db4ce7d0cd27eea1c5044d92a239
andrii.shmerchuk <andrii.shmerchuk@moebel.de>
2023-10-16 16:57:53 +0200

    Added query to showing free desks

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -55,13 +55,34 @@ class RoomController extends AbstractController
             return $this->redirectToRoute('login');
         }
 
+        $sql = "SELECT d.id AS desk_id
+                FROM desks d
+                LEFT JOIN reservations r ON d.id = r.desk_id AND r.reservation_time = '$pikedDate'
+                WHERE d.room_id = '$roomId'
+                AND r.id IS NULL";
+        $stmt = $conn->prepare($sql);
+        $result = $conn->query($sql);
+
+        $desksId = array(); // Initialize an array to store desk IDs
+
+        if ($result) {
+            while ($row = $result->fetch_assoc()) {
+                $desksId[] = $row['desk_id']; // Add each desk_id to the array
+            }
+        } else {
+            return $this->redirectToRoute('login');
+        }
+
+//        dd($desksId);
+
 
         return $this->render('custom_templates/desks.html.twig', [
             'userId' => $userId,
             'usersName' => $usersName,
             'email' => $usersEmail,
             'date' => $pikedDate,
-            'roomName' => $roomName
+            'roomName' => $roomName,
+            'desksId' => $desksId
         ]);
     }
 }
