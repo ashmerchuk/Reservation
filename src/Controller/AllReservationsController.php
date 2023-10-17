8a8e7702691e4dd7f9a1148125500be9a2dfd2c9
andrii.shmerchuk <andrii.shmerchuk@moebel.de>
2023-10-17 17:19:09 +0200

    Added delete reservation function

--- src/Controller/AllReservationsController.php
+++ src/Controller/AllReservationsController.php
@@ -63,6 +63,57 @@ class AllReservationsController extends AbstractController
     }
     public function deleteReservations(Request $request, SessionInterface $session): Response
     {
-        dd($request);
+//        dd($request->get('email'));
+//        dd($request->get('reservation_id'));
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            session_start();
+        }
+//            dd('moin');
+        $servername = "reservation-mysql";
+        $username = "root";
+        $password = "test_pass";
+
+// Create connection
+        $conn = new mysqli($servername, $username, $password, 'reservation');
+
+// Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $reservationId = $request->get('reservation_id');
+        $sql = "DELETE FROM reservations WHERE id = $reservationId";
+        $result = $conn->query($sql);
+
+        $userId = $_SESSION['user_id'];
+
+        $sql = "SELECT r.id AS reservation_id, d.name AS desk_name, d.id AS desk_id, r.reservation_time, r.user_id, ro.name AS room_name
+        FROM reservations r
+        INNER JOIN desks d ON r.desk_id = d.id
+        INNER JOIN rooms ro ON d.room_id = ro.id
+        WHERE r.user_id = $userId
+        ";
+
+        $result = $conn->query($sql);
+
+        if ($result) {
+            $reservations = $result->fetch_all(MYSQLI_ASSOC);
+        } else {
+            // Handle the query error
+        }
+
+        $sql = "SELECT name, email FROM users WHERE id = $userId";
+        $result = $conn->query($sql);
+        if($result){
+            $row = $result->fetch_assoc();
+            $userName = $row['name'];
+            $userEmail = $row['email'];
+        }
+
+        return $this->render('custom_templates/allReservations.html.twig', [
+            'reservations' => $reservations, // Pass the reservations data to the template
+            'usersName' => $userName,
+            'email' => $userEmail
+        ]);
+
     }
 }
