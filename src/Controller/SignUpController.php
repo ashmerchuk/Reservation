da3afed4896dda38b1681f9382bcc7b4f7d14d78
andrii.shmerchuk <andrii.shmerchuk@moebel.de>
2024-01-02 13:58:48 +0100

    Fixed hover styling on the confirm button

--- src/Controller/SignUpController.php
+++ src/Controller/SignUpController.php
@@ -9,26 +9,13 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
-
 class SignUpController extends AbstractController
 {
     public function signUp(Request $request, SessionInterface $session): Response
     {
-
         if ($request->getMethod() == 'GET') {
-
-            return $this->render('custom_templates/signUp.html.twig', [
-//                'userId' => $userId,
-//                'usersName' => $usersName,
-//                'email' => $usersEmail,
-//                'rooms' => $rooms
-            ]);
+            return $this->render('custom_templates/signUp.html.twig');
         }
-
-//        dd('moin');
-        $usersEmail = $request->get('signUpEmail');
-        $usersName = $request->get('signUpName');
-        $usersPassword = $request->get('signUpPassword');
 
         $servername = "reservation-mysql";
         $username = "root";
@@ -37,7 +24,7 @@ class SignUpController extends AbstractController
 // Create connection
         $conn = new mysqli($servername, $username, $password, 'reservation');
 
-// Check connection
+        // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
@@ -45,6 +32,9 @@ class SignUpController extends AbstractController
         $createtable = "CREATE TABLE IF NOT EXISTS `users` (`id` int AUTO_INCREMENT, `email` varchar(255), `name` varchar(255), `password` varchar(255), `user_id` varchar(255), PRIMARY KEY (`id`))";
         $result = $conn->query($createtable);
 
+        $usersEmail = $request->get('signUpEmail');
+        $usersName = $request->get('signUpName');
+        $usersPassword = $request->get('signUpPassword');
         $sql = "SELECT email FROM `users` WHERE email = '$usersEmail'";
         $result = $conn->query($sql);
         if($result){
@@ -63,7 +53,6 @@ class SignUpController extends AbstractController
             }
         }
 
-         return new RedirectResponse('login');
+        return new RedirectResponse('login');
     }
-
 }
